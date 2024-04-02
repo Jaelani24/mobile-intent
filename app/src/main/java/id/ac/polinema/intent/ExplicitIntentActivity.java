a88236642091f82f47a3a101c067af137c328d64
Jaelani24 <jthebluess@gmail.com>
2024-04-02 22:14:19 +0700

    Update ExplicitIntentActivity

--- app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -1,15 +1,36 @@
 package id.ac.polinema.intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    private EditText nameInput;
+    private TextView outputText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        nameInput = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
+        Button submitButton = findViewById(R.id.btn_submit);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = nameInput.getText().toString();
+                outputText.setText("Hello " + inputName + " , Congratulations! ");
+            }
+        });
+
     }
 
 }
